bd4d9a90619eec05156a2399c917f59a93908553
Hui Peng Hu <huipengh@captricity.com>
2019-02-05 00:32:00 +0000

    use struct to pack/unpack the header, implement __repr__, provide an "open" alias

--- iidb/__init__.py
+++ iidb/__init__.py
@@ -1,5 +1,6 @@
 import lmdb
 import numpy as np
+import struct
 import zstd
 from typing import Any, Union, Iterable, Tuple
 
@@ -9,12 +10,18 @@ __all__ = ['IIDB']
 class IIDB:
     """Images Interchange Database"""
     def __init__(self, path: str, readonly: bool = True) -> None:
+        self.path = path
+        self.readonly = readonly
         self.env = lmdb.open(path, map_size=1024**4, subdir=False, lock=False, readonly=readonly)
         self.compressor = zstd.ZstdCompressor()
         self.decompressor = zstd.ZstdDecompressor()
+        self.header_packer = struct.Struct('<HHHH')
 
     def close(self):
         self.env.close()
+
+    def __repr__(self) -> str:
+        return f'IIDB(path={self.path!r}, readonly={self.readonly})'
 
     def _compress(self, value):
         height, width = value.shape[:2]
@@ -22,15 +29,15 @@ class IIDB:
             channels = 1
         else:
             channels = value.shape[2]
-        header = np.array([0, height, width, channels], dtype=np.uint16)
+        header_blob = self.header_packer.pack(0, height, width, channels)
         compressed_blob = self.compressor.compress(value.tobytes())
-        return header.tobytes() + compressed_blob
+        return header_blob + compressed_blob
 
     def get(self, key: Union[int, str]):
         with self.env.begin(write=False) as txn:
             value = txn.get(str(key).encode('utf-8'))
 
-        header = np.frombuffer(value[:8], dtype=np.uint16)
+        header = self.header_packer.unpack(value[:8])
         height = header[1]
         width = header[2]
         channels = header[3]
@@ -60,8 +67,11 @@ class IIDB:
     def get_image_dimension(self, key: Union[int, str]) -> Tuple[int, int]:
         with self.env.begin(write=False, buffers=True) as txn:
             buf = txn.get(str(key).encode('utf-8'))
-            header = np.frombuffer(buf[:8], dtype=np.uint16)
-            height = int(header[1])
-            width = int(header[2])
+            header = self.header_packer.unpack(buf[:8])
+            height = header[1]
+            width = header[2]
 
         return (height, width)
+
+
+open = IIDB
